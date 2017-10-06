18db8fb284d331ee109a20f13c64e8ea22c1496b
Artem Sokovets <relix@mail.ru>
2017-10-06 23:50:05 +0300

    add new package

---
+++ src/test/java/ParallelStreams/easy/TestCaseEasy.java
@@ -0,0 +1,9 @@
+package ParallelStreams.easy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestCaseEasy {
+
+    private static Logger logger = LoggerFactory.getLogger(ParallelStreams.easy.TestCaseEasy.class);
+}
