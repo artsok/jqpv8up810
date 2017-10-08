b4ee736c763696bf9d86241e52dee4c872e17cb8
Artem Sokovets <relix@mail.ru>
2017-10-08 23:09:02 +0300

    add new package

--- src/test/java/ParallelStreams/easy/TestCaseEasy.java
+++ src/test/java/ParallelStreams/easy/TestCaseEasy.java
@@ -1,9 +1,26 @@
 package ParallelStreams.easy;
 
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class TestCaseEasy {
 
     private static Logger logger = LoggerFactory.getLogger(ParallelStreams.easy.TestCaseEasy.class);
+
+    /**
+     * This code illustrates how you can take advantage of multiple cores and still preserve the order of the output.
+     * Note that forEachOrdered forces the iteration of the elements of the stream in an ordered fashion.
+     * However, any operation that is chained before forEachOrdered will still happen in parallel because the stream
+     * is a parallel stream. Therefore, the map function will be executed by mutliple
+     * threads on different elements of the stream in parallel taking advantage of multiple CPU cores.
+     */
+    @Test
+    public void testForEachOrdered() {
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4);
+        integers.stream().parallel().forEachOrdered(a -> logger.info("Число в массиве '{}'", a));
+    }
 }
--- src/test/java/javacollectionandstreamswithlambdas/easy/TestCaseEasy.java
+++ src/test/java/javacollectionandstreamswithlambdas/easy/TestCaseEasy.java
@@ -1,6 +1,5 @@
 package javacollectionandstreamswithlambdas.easy;
 
-import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
